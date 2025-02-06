1f312fe0f844559857ff3f9f53a27f4527fc57bb
Zeyad Moustafa Kamal <zeyad20062018@gmail.com>
2025-02-06 22:10:36 +0200

    feat: add a command line interface

--- src/cli.py
+++ src/cli.py
@@ -1,10 +1,46 @@
 from anime import Anime
 
-def main():
-    url = "https://witanime.quest/episode/ao-no-hako-%d8%a7%d9%84%d8%ad%d9%84%d9%82%d8%a9-6/"
+import typer
+
+from typing import Annotated
+
+app = typer.Typer()
+
+
+@app.command()
+def download(
+    url: str,
+    episodes: Annotated[
+        str,
+        typer.Option(
+            "-e",
+            "--episodes",
+            help="The episodes line. You should pass the number of the episode to download \
+or you can pass something like 1-5 to download from the episode number 1 to the number 5. Will be ignored if --all is passed",
+        ),
+    ] = None,
+    download_all: Annotated[
+        bool,
+        typer.Option(
+            "--all",
+            help="Download all episodes of the anime. Note that this is the default option if not --line",
+        ),
+    ] = None,
+) -> None:
+    # url = "https://witanime.quest/episode/ao-no-hako-%d8%a7%d9%84%d8%ad%d9%84%d9%82%d8%a9-6/"
+    if download_all:
+        episodes = "all"
+    else:
+        if not episodes:
+            episodes = "all"
     anime = Anime(url)
-    episodes_line = "12-17"
-    anime.download(episodes_line)
+    anime.download(episodes)
+
+
+@app.command()
+def info(url: str) -> None:
+    print(url)
+
 
 if __name__ == "__main__":
-    main()
+    app()
